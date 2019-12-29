d43af800e7024df5e20cfffc02378ab5ec68f8a9
Baiketli Dijo <baiketlid@gmail.com>
2019-12-29 10:15:17 +0200

    Getting started with Hello World!

---
+++ src/gettingstarted/HelloWorld.java
@@ -0,0 +1,12 @@
+package gettingstarted;
+
+/**
+ * Typical class for rookies
+ *
+ * @author bad
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
